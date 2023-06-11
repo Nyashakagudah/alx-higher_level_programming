ca29e66ad385988dc17c21ccc20cadf8b6105587
nyashakagudah <kagudagrace@gmail.com>
2023-06-11 09:37:38 -0700

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
-def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return my_list
-    if idx >= len(my_list):
-        return my_list
-    my_list[idx] = element
-    return my_list
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    listlength = len(my_list) - 1
+    if (idx < 0 or idx > listlength):
+        return (None)
+    else:
+        return (my_list[idx])
