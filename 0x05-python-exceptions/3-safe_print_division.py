345b7902969fc388839772afa72f7a700e7fff9c
nyashakagudah <kagudagrace@gmail.com>
2023-06-26 12:12:57 -0700

    3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 # safe_division.py
-# Calculates division of a by b, catching potential errors
 
-def safe_print_division(num1, num2):
-    """Returns the division of num1 by num2, or None if an error occurred."""
+def safe_print_division(a, b):
+    '''
+    Divide two integers and print the result, using except to catch zero division error
+    '''
     try:
-        result = num1 / num2
-    except (TypeError, ZeroDivisionError):
+        result = a/b
+    except:
         result = None
     finally:
-        print(f"Result: {result}")
-    return result
+        print("The result is: {}".format(result))
+        return result
 
